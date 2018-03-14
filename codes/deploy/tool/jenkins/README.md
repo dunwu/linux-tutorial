cd45305a119e37056970bba2c768f2da242b5141
Zhang Peng <forbreak@163.com>
2018-03-14 15:02:27 +0800

    :bookmark: jenkins 安装配置

---
+++ codes/deploy/tool/jenkins/README.md
@@ -0,0 +1,13 @@
+# 安装 Jenkins
+
+## 安装 Jenkins8
+
+使用方法：
+
+```sh
+wget --no-check-certificate --no-cookies https://raw.githubusercontent.com/dunwu/linux/master/codes/deploy/tool/jenkins/install-jenkins.sh
+chmod -R 777 install-jenkins.sh
+./install-jenkins.sh
+```
+
+脚本会下载解压 Jenkins 到 `/opt/software/jenkins` 路径下。
---
+++ codes/deploy/tool/jenkins/install-jenkins.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+###################################################################################
+# 安装 Jenkins 脚本
+# 适用于所有 linux 发行版本。
+# Author: Zhang Peng
+###################################################################################
+
+echo -e "\n>>>>>>>>> install jenkins"
+
+# 下载并解压 jenkins
+mkdir -p /opt/software/jenkins
+wget -O /opt/software/jenkins/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
