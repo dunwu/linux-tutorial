63047dbe646a8ab7448046cf43a08238ce0222c4
Zhang Peng <forbreak@163.com>
2018-07-12 13:50:10 +0800

    :bookmark: ZooKeeper

---
+++ codes/deploy/tool/zookeeper/README.md
@@ -0,0 +1,11 @@
+# 安装 ZooKeeper
+
+使用方法：
+
+```sh
+wget --no-check-certificate --no-cookies https://raw.githubusercontent.com/dunwu/linux/master/codes/deploy/tool/zookeeper/install-zookeeper.sh
+chmod -R 777 install-zookeeper.sh
+./install-zookeeper.sh
+```
+
+脚本会下载解压 zookeeper 到 `/opt/software/zookeeper` 路径下。
---
+++ codes/deploy/tool/zookeeper/install-zookeeper.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+###################################################################################
+# 安装 zookeeper 脚本
+# 适用于所有 linux 发行版本。
+# Author: Zhang Peng
+###################################################################################
+
+echo -e "\n>>>>>>>>> install zookeeper"
+
+root=/opt/software/zookeeper
+version=3.4.12
+mkdir -p ${root}
+wget -O ${root}/zookeeper-${version}.tar.gz http://mirrors.hust.edu.cn/apache/zookeeper/zookeeper-${version}/zookeeper-${version}.tar.gz
+cd ${root}
+tar -zxf zookeeper-${version}.tar.gz
+cd zookeeper-${version}
