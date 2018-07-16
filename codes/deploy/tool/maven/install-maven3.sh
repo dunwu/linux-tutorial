622c78d13da8c54273af41e6111749ccaeb25fd6
Zhang Peng <forbreak@163.com>
2018-07-16 19:16:39 +0800

    :arrow_up: 下载镜像地址中的maven版本失效，更新版本

--- codes/deploy/tool/maven/install-maven3.sh
+++ codes/deploy/tool/maven/install-maven3.sh
@@ -13,7 +13,7 @@ echo -e "\n>>>>>>>>> install maven"
 mkdir -p /opt/software/maven
 cd /opt/software/maven
 
-version=3.5.2
+version=3.2.5
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz
 tar -zxvf apache-maven-${version}-bin.tar.gz
 
