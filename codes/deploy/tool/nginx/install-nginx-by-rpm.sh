4cd8db7735e871e8fcc6b1f26a1bfa4c986c7f29
Zhang Peng <forbreak@163.com>
2018-09-29 18:28:47 +0800

    :bookmark: Nginx rpm 安装脚本

---
+++ codes/deploy/tool/nginx/install-nginx-by-rpm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+###################################################################################
+# 安装 nginx 脚本
+# 仅适用于 centos 发行版本。
+# Author: Zhang Peng
+###################################################################################
+
+echo -e "\n>>>>>>>>> install nginx"
+
+# Centos7 rpm 地址：http://nginx.org/packages/rhel/7/x86_64/RPMS/
+# CentOS6 rpm 地址：http://nginx.org/packages/rhel/6/x86_64/RPMS/
+
+mkdir -p /opt/nginx
+wget -O /opt/nginx/nginx.rpm http://nginx.org/packages/rhel/7/x86_64/RPMS/nginx-1.14.0-1.el7_4.ngx.x86_64.rpm
+rpm -ivh /opt/nginx/nginx.rpm
+nginx -v
