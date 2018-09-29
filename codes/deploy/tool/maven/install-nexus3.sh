68e9bb7d8d1d5f4b835b9e6cf170975cf8f77a76
Zhang Peng <forbreak@163.com>
2018-09-29 17:27:31 +0800

    :bookmark: Nexus3 安装脚本

---
+++ codes/deploy/tool/maven/install-nexus3.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+###################################################################################
+# 安装 sonatype nexus(用于搭建 maven 私服) 脚本
+# 适用于所有 linux 发行版本。
+# sonatype nexus 会被安装到 /opt/maven 路径。
+# 注意：sonatype nexus 要求必须先安装 JDK
+# Author: Zhang Peng
+###################################################################################
+
+echo -e "\n>>>>>>>>> install sonatype nexus"
+
+mkdir -p /opt/maven
+cd /opt/maven
+
+version=3.13.0-01
+wget -O /opt/maven/nexus-unix.tar.gz http://download.sonatype.com/nexus/3/nexus-${version}-unix.tar.gz
+tar -zxf nexus-unix.tar.gz
