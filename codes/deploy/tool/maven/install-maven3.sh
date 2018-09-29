ad48adce61facb23ea03590763d98a69a6e81ec7
Zhang Peng <forbreak@163.com>
2018-09-29 10:31:06 +0800

    :pencil2: 修改版本

--- codes/deploy/tool/maven/install-maven3.sh
+++ codes/deploy/tool/maven/install-maven3.sh
@@ -13,7 +13,7 @@ echo -e "\n>>>>>>>>> install maven"
 mkdir -p /opt/maven
 cd /opt/maven
 
-version=3.2.5
+version=3.5.4
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz
 tar -zxvf apache-maven-${version}-bin.tar.gz
 
