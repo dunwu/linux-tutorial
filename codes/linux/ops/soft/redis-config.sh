c40997633b9d83a7661aa37d0e7ad48be4d96d9c
Zhang Peng <forbreak@163.com>
2019-05-07 17:20:50 +0800

    更新脚本

---
+++ codes/linux/ops/soft/redis-config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cat << EOF
+
+###################################################################################
+# 配置 Redis 脚本
+# @system: 适用于 CentOS
+# @author: Zhang Peng
+###################################################################################
+
+EOF
+
+path=/opt/redis/redis-5.0.4
+if [[ -n $1 ]]; then
+  path=$1
+fi
+
+echo -e "\n>>>>>>>>> config redis"
+cp ${path}/redis.conf ${path}/redis.conf.default
+wget -N https://raw.githubusercontent.com/dunwu/os-tutorial/master/codes/linux/ops/soft/config/redis-remote-access.conf -O ${path}/redis.conf
