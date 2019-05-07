a72a40b6124332e26016d1e155260db065ce6c77
Zhang Peng <forbreak@163.com>
2019-05-07 11:08:13 +0800

    更新脚本

---
+++ codes/linux/ops/service/redis-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+cat << EOF
+###################################################################################
+# 安装 Redis 脚本
+# Author: Zhang Peng
+###################################################################################
+EOF
+
+version=5.0.4
+if [[ -n $1 ]]; then
+  version=$1
+fi
+
+root=/opt/redis
+if [[ -n $2 ]]; then
+  root=$2
+fi
+
+echo -e "\n>>>>>>>>> install libs"
+yum install -y zlib zlib-devel gcc-c++ libtool openssl openssl-devel tcl
+
+echo -e "\n>>>>>>>>> download redis"
+mkdir -p ${root}
+wget -O ${root}/redis-${version}.tar.gz http://download.redis.io/releases/redis-${version}.tar.gz
+
+echo -e "\n>>>>>>>>> install redis"
+tar zxvf ${root}/redis-${version}.tar.gz -C ${root}
+cd ${root}/redis-${version}
+make & make install
+cd -
--- codes/linux/ops/service/redis/README.md
+++
@@ -1,9 +0,0 @@
-# 安装 Redis
-
-使用方法：
-
-```sh
-curl -o- https://raw.githubusercontent.com/dunwu/os-tutorial/master/codes/linux/ops/service/redis/install-redis.sh | bash
-```
-
-脚本会下载解压 redis 到 `/opt/redis` 路径下。
