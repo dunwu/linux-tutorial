0da2791a7329016d0a0ce31c80d4930af27510f2
Zhang Peng <forbreak@163.com>
2020-04-02 10:07:17 +0800

    update zsh & oh-my-zsh install scripts

--- codes/linux/soft/zsh-install.sh
+++ codes/linux/soft/zsh-install.sh
@@ -40,7 +40,10 @@ command -v git > /dev/null 2>&1 || {
 yum install -y zsh
 chsh -s /bin/zsh
 # install oh-my-zsh
-curl -o- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+# 由于国内经常无法使用 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# 所以，索性将安装脚本下载下来直接使用
+#curl -o- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+zsh/oh-my-zsh-install.sh
 # choose oh-my-zsh theme
 sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\"ys\"/g" ~/.zshrc
 # install oh-my-zsh plugins
@@ -48,6 +51,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plug
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 sed -i "s/^plugins=.*/plugins=(git z wd extract zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
 # reload zsh
-
+# 注册到 /etc/shells
+echo "/usr/bin/zsh" >> /etc/shells
+# 切换 shell
+chsh -s $(which zsh)
 printf "${GREEN}<<<<<<<< install zsh finished${RESET}\n"
 printf "${GREEN}Please reboot to take effect.${RESET}\n"
